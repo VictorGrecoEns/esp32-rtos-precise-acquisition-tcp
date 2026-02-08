a9a5f230920fce255df249e7655ce4fd85802908
Victor GRECO <victor.greco@ens-rennes.fr>
2026-02-08 19:04:23 +0100

    Ignore backup directory

--- backup/07022026/test_acqu_serie/wifi_config.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <Arduino.h>
-
-void connect_wifi();
-void get_blocks_from_server();
-void send_init_to_server(uint32_t fe, uint16_t block_size, uint32_t n_blocks);
-
-extern const char* serverUrl;
-extern volatile int nbBlocToSend;
